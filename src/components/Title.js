92d95e10508bddb288b9239140dde371d734492d
Ernur3021 <ernuromarer@gmail.com>
2023-08-28 15:01:21 +0600

    propst fix

---
+++ src/components/Title.js
@@ -0,0 +1,17 @@
+
+
+
+
+ const Title = () => {
+  return( 
+    <div>
+      <h1 className="title"> Kingdom Events </h1>
+      <br />
+      <p className="subtitle"> All events well be here </p>
+    </div>
+  )
+ }
+
+
+
+export default Title  
